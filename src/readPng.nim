066e9b222c5921067d90d0510e2fecc293afcc09
RiRyCasals <rirycasals@gmail.com>
2022-02-27 18:26:53 +0900

    IHDRのチャンクサイズ取得から画像の高さまでの16バイト分の読み込み部を作成

--- src/readPng.nim
+++ src/readPng.nim
@@ -1,5 +1,6 @@
 import
-  os
+  os,
+  strformat
 
 #[ 画像を配列として読み込む手順（予定）http://www.snap-tck.com/room03/c02/cg/cg07_02.html
   1. pngであることの確認 -> PNGヘッダ
@@ -27,6 +28,39 @@ proc isPngImage(buffer: array[8, uint8]): bool =
     return false
   return true
 
+proc get4BytesToInt(file: File): int =
+  var buffer: array[4, uint8]
+  discard file.readBytes(buffer, 0, 4)
+  result = int(buffer[0].shl(24) + buffer[1].shl(16) + buffer[2].shl(8) + buffer[3])
+
+#常に13らしいのでそこのチェックを入れる
+proc getChunkDataSize(file: File): int =
+  result = file.get4BytesToInt
+  if result != 13:
+    quit("Chunk data size is not 13 byte", QuitFailure)
+
+proc isIHDR(buffer: array[4, uint8]): bool =
+  if buffer[0] != 0x49:
+    return false
+  if buffer[1] != 0x48:
+    return false
+  if buffer[2] != 0x44:
+    return false
+  if buffer[3] != 0x52:
+    return false
+  return true
+
+proc readImageHeaderChunk(file: File) =
+  let chunkDataSize = file.getChunkDataSize
+  echo fmt"chunk data size: {chunkDataSize}"
+  var chunkType: array[4, uint8]
+  discard file.readBytes(chunkType, 0, 4)
+  echo fmt"chunk type: {chunkType[0]:#x} {chunkType[1]:#x} {chunkType[2]:#x} {chunkType[3]:#x}"
+  if not chunkType.isIHDR:
+    quit("pngファイルシグネチャの後にIHDRがありません", QuitFailure)
+  let width = file.get4BytesToInt
+  let height = file.get4BytesToInt
+  echo fmt"w:{width:>6}, h:{height:>6}"
 
 proc loadImage(path: string) =
   block:
@@ -42,6 +76,7 @@ proc loadImage(path: string) =
       echo "file is not png format"
       return
     echo "this is png file!"
+    file.readImageHeaderChunk
 
 
 when isMainModule:
